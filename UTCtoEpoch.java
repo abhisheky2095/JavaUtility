ede79f6345bddefffc563cd3f2f70dc5acdaf1db
abhisheky2095 <abhisheky2095@gmail.com>
2018-10-11 13:27:55 +0530

    Update UTCtoEpoch.java

--- UTCtoEpoch.java
+++ UTCtoEpoch.java
@@ -9,7 +9,7 @@ public class UTCtoEpoch {
 	public static void main(String args[]) throws ParseException {
 		String timezone = "2018-09-20T06:31:02.072Z";    //also can be input from user
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-	    df.setTimeZone(TimeZone.getTimeZone("UTC"));
+	        df.setTimeZone(TimeZone.getTimeZone("UTC"));
 		Date date = df.parse(timezone);
 		long epoch = date.getTime()/1000;          //here time in epoch seconds.
 		
